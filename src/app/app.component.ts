dfb21988d5c97478d0fe92535b0ee0f7f9308c84
sathiyabharathi <sathiyabharathi.paramasivam@gmail.com>
2025-11-09 14:43:57 +0530

    Added photes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: 
+  './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Sid-App';
+  photos = [
+  { url: 'assets/photos/1.jpg', caption: 'Family Picnic' },
+  { url: 'assets/photos/2.jpg', caption: 'Birthday Celebration' },
+  { url: 'assets/photos/3.jpg', caption: 'Birthday Celebration' },
+  { url: 'assets/photos/4.jpg', caption: 'Birthday Celebration' },
+  // Add more photos here
+];
 }
